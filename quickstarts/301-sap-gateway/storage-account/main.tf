d88d7179074f7a2716dda8eb1dbb482108347d67
Eduardo Sanchez <eduardodfmex@hotmail.com>
2024-02-05 10:59:37 -0600

    Adding log analytics workspace.

--- quickstarts/301-sap-gateway/storage-account/main.tf
+++ quickstarts/301-sap-gateway/storage-account/main.tf
@@ -105,4 +105,20 @@ resource "azurerm_storage_blob" "storage_blob_runtime_setup" {
   source                 = "/workspaces/terraform-provider-power-platform/examples/quickstarts/301-sap-gateway/storage-account/scripts/runtime-setup.ps1"
 }
 
+### Loging for Storageaccount blob
 
+resource "azurerm_log_analytics_workspace" "analytics_workspace_sapinteration" {
+  name                = "sapintegration-workspace"
+  location            = var.region
+  resource_group_name = var.resource_group_name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+resource "azurerm_log_analytics_storage_insights" "analytics_storage_insights_sapintegration" {
+  name                = "sapintegration-storageinsightconfig"
+  resource_group_name = var.resource_group_name
+  workspace_id        = azurerm_log_analytics_workspace.analytics_workspace_sapinteration.id
+  storage_account_id  = azurerm_storage_account.storage_account.id
+  storage_account_key = azurerm_storage_account.storage_account.primary_access_key
+  blob_container_names= [azurerm_storage_container.storage_container_installs.name]
+}
