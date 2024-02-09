f69fe03239e8826abbc273093b65b5849c121b24
Eduardo Sanchez <eduardodfmex@hotmail.com>
2024-02-09 12:53:45 -0600

    Running terraform format.

--- quickstarts/301-sap-gateway/main.tf
+++ quickstarts/301-sap-gateway/main.tf
@@ -143,7 +143,7 @@ resource "azurerm_network_interface" "nic" {
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "internal"
+    name = "internal"
     #subnet_id                     = var.sap_subnet_id
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
