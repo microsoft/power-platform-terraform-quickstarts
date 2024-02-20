8c126de354e82aa6a4e5b147c5e4e4797c80cb68
Eduardo Sanchez <eduardodfmex@hotmail.com>
2024-02-20 10:41:50 -0600

    REmoving currency_code.

--- quickstarts/000-blank-sample/main.tf
+++ quickstarts/000-blank-sample/main.tf
@@ -22,7 +22,7 @@ resource "powerplatform_environment" "development" {
   display_name      = "example_environment"
   location          = "europe"
   language_code     = "1033"
-  currency_code     = "USD"
+  //currency_code     = "USD"
   environment_type  = "Sandbox"
   //domain            = "mydomain"
   security_group_id = ""//"00000000-0000-0000-0000-000000000000"
