0d25c778e6ff6e6c3a67c28e2119463e17b46f75
mawasile <50197777+mawasile@users.noreply.github.com>
2024-02-21 13:58:57 +0100

    merging back pipeline dev&test (#17)
    * Update tf-unit-tests.yml
    
    The command "terraform fmt -check" show the list of filenames whose files are not properly formatted. but don't format those files and the action fails.
    Command "terraform fmt -recursive" will format those files if needed, same command that the PR owner will need to run before commit changes.
    
    * Mawasile/general fixes (#1)
    
    * WIP examples & testing
    
    * updating examples
    
    * Updating terraform resources.
    
    * formating tf files.
    
    * Removing tf lock.
    
    * Fixing MD problems.
    
    * Fixing typo.
    
    * Adding word to dictionary.
    
    * refactoring bootstrap logic
    
    * adding 101 example (WIP)
    
    ---------
    
    Co-authored-by: Eduardo Sanchez <eduardodfmex@hotmail.com>
    
    * wip pipeline example
    
    * pipeline test1
    
    * pipeline test2
    
    * pipeline test3
    
    * pipeline test4
    
    * pipeline test5
    
    * pipeline test 6
    
    * pipeline test 7
    
    * test pipeline test 8
    
    * test pipeline test 9
    
    * test pipeline test 10
    
    * test pipeline 11
    
    * Update display name in powerplatform_environment resource
    
    * pipeline destroy test
    
    * Update main.tf (#2)
    
    * Update main.tf (#3)
    
    * 102 ggithub pipeline example
    
    ---------
    
    Co-authored-by: Eduardo Sanchez <eduardodfmex@hotmail.com>

--- examples/102-github-pipeline/main.tf
+++ examples/102-github-pipeline/main.tf
@@ -7,19 +7,21 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "value"
-    storage_account_name = "value"
-    container_name       = "tfstate"
-    key                  = "pipeline-example.terraform.tfstate"
+    container_name = "tfstate"
+    key            = "pipeline-example.terraform.tfstate"
   }
 }
 
-resource "powerplatform_environment" "dev" {
-  location          = "unitedstates"
-  language_code     = 1033
-  display_name      = "pipeline-example"
-  currency_code     = "USD"
-  environment_type  = "Sandbox"
-  domain            = "pipeline-example"
-  security_group_id = "00000000-0000-0000-0000-000000000000"
+provider "powerplatform" {
 }
+
+ resource "powerplatform_environment" "dev" {
+   location          = "unitedstates"
+   language_code     = 1033
+   display_name      = "pipeline-example"
+   currency_code     = "USD"
+   environment_type  = "Sandbox"
+   domain            = "pipeline-example"
+   security_group_id = "00000000-0000-0000-0000-000000000000"
+ }
+
