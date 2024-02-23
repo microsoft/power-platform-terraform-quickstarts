3d046e9515b8594f346467005a0af3e408bb4452
Mateusz Wasilewski <50197777+mawasile@users.noreply.github.com>
2024-02-23 11:57:32 +0100

    version 7.12 bump

--- examples/102-github-pipeline/main.tf
+++ examples/102-github-pipeline/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     powerplatform = {
       source  = "microsoft/power-platform"
-      version = "0.7.8-preview"
+      version = "0.7.12-preview"
     }
   }
 
