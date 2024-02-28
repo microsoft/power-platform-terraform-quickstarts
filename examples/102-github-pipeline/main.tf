4d8a57dbdf502071242b95ddbcb7a7cbcc6ab179
mawasile <50197777+mawasile@users.noreply.github.com>
2024-02-28 14:39:43 +0100

    Update main.tf

--- examples/102-github-pipeline/main.tf
+++ examples/102-github-pipeline/main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
     container_name = "tfstate"
-    key            = "pipeline-example-test.terraform.tfstate"
+    key            = "pipeline-example-test3.terraform.tfstate"
     use_oidc = true
   }
 }
@@ -20,7 +20,7 @@ provider "powerplatform" {
   resource "powerplatform_environment" "dev" {
     location          = "unitedstates"
     language_code     = 1033
-    display_name      = "pipeline-example-test1"
+    display_name      = "pipeline-example-test3"
     currency_code     = "USD"
     environment_type  = "Sandbox"
     security_group_id = "00000000-0000-0000-0000-000000000000"
