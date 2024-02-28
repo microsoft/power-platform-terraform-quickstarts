883d5e270a947312598ec440b18f23d90ddc85c5
mawasile <50197777+mawasile@users.noreply.github.com>
2024-02-28 22:23:13 +0100

    Update main.tf update

--- examples/102-github-pipeline/main.tf
+++ examples/102-github-pipeline/main.tf
@@ -16,11 +16,19 @@ terraform {
 provider "powerplatform" {
   use_oidc = true
 }
-
   resource "powerplatform_environment" "dev" {
     location          = "unitedstates"
     language_code     = 1033
-    display_name      = "demo-dev"
+    display_name      = "demo-dev-update"
+    currency_code     = "USD"
+    environment_type  = "Sandbox"
+    security_group_id = "00000000-0000-0000-0000-000000000000"
+  }
+
+  resource "powerplatform_environment" "test" {
+    location          = "unitedstates"
+    language_code     = 1033
+    display_name      = "demo-test"
     currency_code     = "USD"
     environment_type  = "Sandbox"
     security_group_id = "00000000-0000-0000-0000-000000000000"
