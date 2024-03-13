6f24793014466dc77ab917daa2c46f8e0da96765
Mateusz Wasilewski <50197777+mawasile@users.noreply.github.com>
2024-03-13 12:05:36 +0000

    Add Azure AD user and group for Dev Access***

--- quickstarts/102-github-pipeline/main.tf
+++ quickstarts/102-github-pipeline/main.tf
@@ -17,12 +17,31 @@ provider "powerplatform" {
   use_oidc = true
 }
 
- resource "powerplatform_environment" "dev" {
-   location          = "unitedstates"
-   language_code     = 1033
-   display_name      = "pipeline-example"
-   currency_code     = "USD"
-   environment_type  = "Sandbox"
-   security_group_id = "00000000-0000-0000-0000-000000000000"
- }
+resource "azuread_user" "user1" {
+  user_principal_name = "user1@mateuszwasilewskihotmail.onmicrosoft.com"
+  display_name        = "User One"
+  mail_nickname       = "user1"
+}
+
+resource "azuread_group" "dev_access" {
+  display_name = "Dataverse Dev Access"
+  description  = "Dataverse Dev Access Group for Power Platform"
+  mail_enabled = false
+  security_enabled = true
+}
+
+resource "azuread_group_member" "user1_member" {
+  group_object_id = azuread_group.dev_access.id
+  member_object_id = azuread_user.user1.id
+}
+
+
+resource "powerplatform_environment" "dev" {
+  location          = "unitedstates"
+  language_code     = 1033
+  display_name      = "pipeline-example"
+  currency_code     = "USD"
+  environment_type  = "Sandbox"
+  security_group_id = azuread_group.dev_access.id
+}
 
