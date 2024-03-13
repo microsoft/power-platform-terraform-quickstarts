2290119826e0dd86c5193dac2037ff2b38c57d2d
Mateusz Wasilewski <50197777+mawasile@users.noreply.github.com>
2024-03-13 13:51:41 +0000

    Add Azure AD users and groups for Dev environment access

--- quickstarts/102-github-pipeline/main.tf
+++ quickstarts/102-github-pipeline/main.tf
@@ -31,6 +31,10 @@ data "azuread_domains" "aad_domains" {
   only_initial = true
 }
 
+data "powerplatform_securityroles" "all" {
+  environment_id = powerplatform_environment.dev.id
+}
+
 locals {
   domain_name = data.azuread_domains.aad_domains.domains[0].domain_name
 }
@@ -41,50 +45,72 @@ resource "random_password" "passwords" {
   override_special = "_%@"
 }
 
-# resource "azuread_user" "dev_user1" {
-#   user_principal_name = "dev1@${local.domain_name}"
-#   display_name        = "Dev User1"
-#   mail_nickname       = "dev1"
-#   password = random_password.passwords.result
-#   usage_location = "US"
-# }
+locals {
+  dev_users = tolist([ "dev1", "dev2" ])
+}
 
-# resource "azuread_group" "dev_access" {
-#   display_name = "Dataverse Dev Access"
-#   description  = "Dataverse Dev Access Group for Power Platform"
-#   mail_enabled = false
-#   security_enabled = true
-# }
+resource "azuread_user" "dev_user" {
+  count = length(local.dev_users)
+  user_principal_name = "${local.dev_users[count.index]}@${local.domain_name}"
+  display_name        = local.dev_users[count.index]
+  mail_nickname       = local.dev_users[count.index]
+  password = random_password.passwords.result
+  usage_location = "US"
+}
+
+resource "azuread_group" "dev_access" {
+  display_name = "Dataverse Dev Environment Access"
+  description  = "Dataverse Dev Environment Access Group for Power Platform"
+  mail_enabled = false
+  security_enabled = true 
+  members = azuread_user.dev_user[*].object_id
+}
+
+resource "powerplatform_environment" "dev" {
+  location          = "unitedstates"
+  language_code     = 1033
+  display_name      = "dev-main"
+  currency_code     = "USD"
+  environment_type  = "Sandbox"
+  security_group_id = azuread_group.dev_access.id
+}
+
+resource "powerplatform_user" "new_user" {
+  count = length(local.dev_users)
+  environment_id = powerplatform_environment.dev.id
+  security_roles = local.developer_roles
+  aad_id = azuread_user.dev_user[count.index].id
+}
+
+locals {
+  developer_roles = toset([for role in data.powerplatform_securityroles.all.security_roles : role.role_id if 
+    role.name == "System Customizer" || 
+    role.name ==  "Environment Maker"
+  ])
+  local_dev_env_roles = toset([for role in data.powerplatform_securityroles.all.security_roles : role.role_id if 
+    role.name == "System Administrator"
+  ])
+}
+
+resource "powerplatform_environment" "user_dev_env" {
+  count = length(local.dev_users)
+  location          = "unitedstates"
+  language_code     = 1033
+  display_name      = "${dev_users[count.index]}-development-env"
+  currency_code     = "USD"
+  environment_type  = "Sandbox"
+  security_group_id = azuread_group.dev_access.id
+}
+
+resource "powerplatform_user" "user_dev_env" {
+  count = length(local.dev_users)
+  environment_id = powerplatform_environment.user_dev_env[count.index].id
+  security_roles = local.local_dev_env_roles
+  aad_id = azuread_user.dev_user[count.index].id
+}
 
 
-# resource "azuread_group_member" "user1_member" {
-#   group_object_id = azuread_group.dev_access.id
-#   member_object_id = azuread_user.dev_user1.id
-# }
 
-# resource "powerplatform_environment" "dev" {
-#   location          = "unitedstates"
-#   language_code     = 1033
-#   display_name      = "pipeline-example"
-#   currency_code     = "USD"
-#   environment_type  = "Sandbox"
-#   security_group_id = azuread_group.dev_access.id
-#   depends_on = [ azuread_group_member.user1_member ]
-# }
 
-# data "powerplatform_securityroles" "all" {
-#   environment_id = powerplatform_environment.dev.id
-# }
 
-# locals {
-#   developer_roles = toset([for role in data.powerplatform_securityroles.all.security_roles : role.role_id if 
-#     role.name == "System Customizer" || 
-#     role.name ==  "Environment Maker"
-#   ])
-# }
 
-# resource "powerplatform_user" "new_user" {
-#   environment_id = powerplatform_environment.dev.id
-#   security_roles = local.developer_roles
-#   aad_id = azuread_user.dev_user1.id
-# }
