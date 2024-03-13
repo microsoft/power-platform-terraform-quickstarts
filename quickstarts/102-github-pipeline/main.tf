4c393a3d60ede701e9a15213eb12be992da6727a
Mateusz Wasilewski <50197777+mawasile@users.noreply.github.com>
2024-03-13 13:30:27 +0000

    Remove commented out code for dev_user1 and dev_access

--- quickstarts/102-github-pipeline/main.tf
+++ quickstarts/102-github-pipeline/main.tf
@@ -41,50 +41,50 @@ resource "random_password" "passwords" {
   override_special = "_%@"
 }
 
-resource "azuread_user" "dev_user1" {
-  user_principal_name = "dev1@${local.domain_name}"
-  display_name        = "Dev User1"
-  mail_nickname       = "dev1"
-  password = random_password.passwords.result
-  usage_location = "US"
-}
+# resource "azuread_user" "dev_user1" {
+#   user_principal_name = "dev1@${local.domain_name}"
+#   display_name        = "Dev User1"
+#   mail_nickname       = "dev1"
+#   password = random_password.passwords.result
+#   usage_location = "US"
+# }
 
-resource "azuread_group" "dev_access" {
-  display_name = "Dataverse Dev Access"
-  description  = "Dataverse Dev Access Group for Power Platform"
-  mail_enabled = false
-  security_enabled = true
-}
+# resource "azuread_group" "dev_access" {
+#   display_name = "Dataverse Dev Access"
+#   description  = "Dataverse Dev Access Group for Power Platform"
+#   mail_enabled = false
+#   security_enabled = true
+# }
 
 
-resource "azuread_group_member" "user1_member" {
-  group_object_id = azuread_group.dev_access.id
-  member_object_id = azuread_user.dev_user1.id
-}
+# resource "azuread_group_member" "user1_member" {
+#   group_object_id = azuread_group.dev_access.id
+#   member_object_id = azuread_user.dev_user1.id
+# }
 
-resource "powerplatform_environment" "dev" {
-  location          = "unitedstates"
-  language_code     = 1033
-  display_name      = "pipeline-example"
-  currency_code     = "USD"
-  environment_type  = "Sandbox"
-  security_group_id = azuread_group.dev_access.id
-  depends_on = [ azuread_group_member.user1_member ]
-}
+# resource "powerplatform_environment" "dev" {
+#   location          = "unitedstates"
+#   language_code     = 1033
+#   display_name      = "pipeline-example"
+#   currency_code     = "USD"
+#   environment_type  = "Sandbox"
+#   security_group_id = azuread_group.dev_access.id
+#   depends_on = [ azuread_group_member.user1_member ]
+# }
 
-data "powerplatform_securityroles" "all" {
-  environment_id = powerplatform_environment.dev.id
-}
+# data "powerplatform_securityroles" "all" {
+#   environment_id = powerplatform_environment.dev.id
+# }
 
-locals {
-  developer_roles = toset([for role in data.powerplatform_securityroles.all.security_roles : role.role_id if 
-    role.name == "System Customizer" || 
-    role.name ==  "Environment Maker"
-  ])
-}
+# locals {
+#   developer_roles = toset([for role in data.powerplatform_securityroles.all.security_roles : role.role_id if 
+#     role.name == "System Customizer" || 
+#     role.name ==  "Environment Maker"
+#   ])
+# }
 
-resource "powerplatform_user" "new_user" {
-  environment_id = powerplatform_environment.dev.id
-  security_roles = local.developer_roles
-  aad_id = azuread_user.dev_user1.id
-}
+# resource "powerplatform_user" "new_user" {
+#   environment_id = powerplatform_environment.dev.id
+#   security_roles = local.developer_roles
+#   aad_id = azuread_user.dev_user1.id
+# }
