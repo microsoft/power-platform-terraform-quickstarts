6944afc924b739ec57ce9f2d141e3f6e1b67d722
Mateusz Wasilewski <50197777+mawasile@users.noreply.github.com>
2024-03-13 14:39:11 +0000

    Add Azure AD user, groups, and Power Platform environments

--- quickstarts/102-github-pipeline/main.tf
+++ quickstarts/102-github-pipeline/main.tf
@@ -46,104 +46,104 @@ locals {
   dev_users = toset([ "dev1", "dev2" ])
 }
 
-# resource "azuread_user" "dev_user" {
-#   count = length(local.dev_users)
-#   user_principal_name = "${local.dev_users[count.index]}@${local.domain_name}"
-#   display_name        = local.dev_users[count.index]
-#   mail_nickname       = local.dev_users[count.index]
-#   password = random_password.passwords.result
-#   usage_location = "US"
-# }
+resource "azuread_user" "dev_user" {
+  count = length(local.dev_users)
+  user_principal_name = "${local.dev_users[count.index]}@${local.domain_name}"
+  display_name        = local.dev_users[count.index]
+  mail_nickname       = local.dev_users[count.index]
+  password = random_password.passwords.result
+  usage_location = "US"
+}
 
-# resource "azuread_group" "dev_access" {
-#   display_name = "Dataverse Dev Environment Access"
-#   description  = "Dataverse Dev Environment Access Group for Power Platform"
-#   mail_enabled = false
-#   security_enabled = true 
-#   members = azuread_user.dev_user[*].object_id
-# }
+resource "azuread_group" "dev_access" {
+  display_name = "Dataverse Dev Environment Access"
+  description  = "Dataverse Dev Environment Access Group for Power Platform"
+  mail_enabled = false
+  security_enabled = true 
+  members = azuread_user.dev_user[*].object_id
+}
 
-# resource "powerplatform_environment" "dev" {
-#   location          = "unitedstates"
-#   language_code     = 1033
-#   display_name      = "dev-main"
-#   currency_code     = "USD"
-#   environment_type  = "Sandbox"
-#   security_group_id = azuread_group.dev_access.id
-# }
+resource "powerplatform_environment" "dev" {
+  location          = "unitedstates"
+  language_code     = 1033
+  display_name      = "dev-main"
+  currency_code     = "USD"
+  environment_type  = "Sandbox"
+  security_group_id = azuread_group.dev_access.id
+}
 
-# data "powerplatform_securityroles" "all" {
-#   environment_id = powerplatform_environment.dev.id
-# }
+data "powerplatform_securityroles" "all" {
+  environment_id = powerplatform_environment.dev.id
+}
 
-# resource "powerplatform_user" "new_user" {
-#   count = length(local.dev_users)
-#   environment_id = powerplatform_environment.dev.id
-#   security_roles = toset([for role in data.powerplatform_securityroles.all.security_roles : role.role_id if 
-#     role.name == "System Customizer" || 
-#     role.name ==  "Environment Maker"
-#   ])
-#   aad_id = azuread_user.dev_user[count.index].id
+resource "powerplatform_user" "new_user" {
+  count = length(local.dev_users)
+  environment_id = powerplatform_environment.dev.id
+  security_roles = toset([for role in data.powerplatform_securityroles.all.security_roles : role.role_id if 
+    role.name == "System Customizer" || 
+    role.name ==  "Environment Maker"
+  ])
+  aad_id = azuread_user.dev_user[count.index].id
 
-#   depends_on = [ azuread_group.dev_access ]
-# }
-
-
-# resource "powerplatform_environment" "user_dev_env" {
-#   count = length(azuread_user.dev_user)
-#   location          = "unitedstates"
-#   language_code     = 1033
-#   display_name      = "${azuread_user.dev_user[count.index].mail_nickname}-private-development"
-#   currency_code     = "USD"
-#   environment_type  = "Sandbox"
-#   security_group_id = azuread_group.dev_access.id
-# }
-
-# data "powerplatform_securityroles" "user_dev_env_roles" {
-#   count = length(powerplatform_environment.user_dev_env)
-#   environment_id = powerplatform_environment.user_dev_env[count.index].id
-# }
-
-# resource "powerplatform_user" "user_dev_env" {
-#   count = length(azuread_user.dev_user)
-#   environment_id = powerplatform_environment.user_dev_env[count.index].id
-#   security_roles = toset([for role in data.powerplatform_securityroles.user_dev_env_roles[count.index].security_roles : role.role_id if 
-#     role.name == "System Administrator"
-#   ])
-#   aad_id = azuread_user.dev_user[count.index].id
-
-#   depends_on = [ azuread_group.dev_access ]
-# }
+  depends_on = [ azuread_group.dev_access ]
+}
 
 
-# resource "azuread_group" "test_access" {
-#   display_name = "Dataverse Test Environment Access"
-#   description  = "Dataverse Test Environment Access Group for Power Platform"
-#   mail_enabled = false
-#   security_enabled = true 
-# }
+resource "powerplatform_environment" "user_dev_env" {
+  count = length(azuread_user.dev_user)
+  location          = "unitedstates"
+  language_code     = 1033
+  display_name      = "${azuread_user.dev_user[count.index].mail_nickname}-private-development"
+  currency_code     = "USD"
+  environment_type  = "Sandbox"
+  security_group_id = azuread_group.dev_access.id
+}
 
-# resource "powerplatform_environment" "test" {
-#   location          = "unitedstates"
-#   language_code     = 1033
-#   display_name      = "test"
-#   currency_code     = "USD"
-#   environment_type  = "Sandbox"
-#   security_group_id = azuread_group.test_access.id
-# }
+data "powerplatform_securityroles" "user_dev_env_roles" {
+  count = length(powerplatform_environment.user_dev_env)
+  environment_id = powerplatform_environment.user_dev_env[count.index].id
+}
 
-# resource "azuread_group" "prod_access" {
-#   display_name = "Dataverse Prod Environment Access"
-#   description  = "Dataverse Prod Environment Access Group for Power Platform"
-#   mail_enabled = false
-#   security_enabled = true 
-# }
+resource "powerplatform_user" "user_dev_env" {
+  count = length(azuread_user.dev_user)
+  environment_id = powerplatform_environment.user_dev_env[count.index].id
+  security_roles = toset([for role in data.powerplatform_securityroles.user_dev_env_roles[count.index].security_roles : role.role_id if 
+    role.name == "System Administrator"
+  ])
+  aad_id = azuread_user.dev_user[count.index].id
 
-# resource "powerplatform_environment" "prod" {
-#   location          = "unitedstates"
-#   language_code     = 1033
-#   display_name      = "prod"
-#   currency_code     = "USD"
-#   environment_type  = "Production"
-#   security_group_id = azuread_group.prod_access.id
-# }
+  depends_on = [ azuread_group.dev_access ]
+}
+
+
+resource "azuread_group" "test_access" {
+  display_name = "Dataverse Test Environment Access"
+  description  = "Dataverse Test Environment Access Group for Power Platform"
+  mail_enabled = false
+  security_enabled = true 
+}
+
+resource "powerplatform_environment" "test" {
+  location          = "unitedstates"
+  language_code     = 1033
+  display_name      = "test"
+  currency_code     = "USD"
+  environment_type  = "Sandbox"
+  security_group_id = azuread_group.test_access.id
+}
+
+resource "azuread_group" "prod_access" {
+  display_name = "Dataverse Prod Environment Access"
+  description  = "Dataverse Prod Environment Access Group for Power Platform"
+  mail_enabled = false
+  security_enabled = true 
+}
+
+resource "powerplatform_environment" "prod" {
+  location          = "unitedstates"
+  language_code     = 1033
+  display_name      = "prod"
+  currency_code     = "USD"
+  environment_type  = "Production"
+  security_group_id = azuread_group.prod_access.id
+}
