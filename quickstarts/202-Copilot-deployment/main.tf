c9e5bd6135f5bc3679ef1cb71c8fc8e8e49fc009
ianjensenisme <ianjensen@microsoft.com>
2024-07-26 17:10:02 -0700

    WIP. Still needs connector add and model adjustments.

--- quickstarts/202-Copilot-deployment/main.tf
+++ quickstarts/202-Copilot-deployment/main.tf
@@ -2,13 +2,15 @@ terraform {
   required_providers {
     powerplatform = {
       source = "microsoft/power-platform"
-      version = ">=2.6.1-preview"
+      version = "2.6.2-preview"
     }
     azurerm = {
       source = "hashicorp/azurerm"
+      version = ">=3.113.0"
     }
-    caf = {
-      source = "aztfmod/caf"
+    azurecaf = {
+      source = "aztfmod/azurecaf"
+      version = ">=1.2.28"
     }
   }
 }
@@ -21,36 +23,47 @@ provider "azurerm" {
   features {}
 }
 
-provider "caf" {}
+provider "azurecaf" {}
 
 #---- 1 - Generate resource names using Azure CAF ----
 
-resource "caf_naming_convention" "resource_group" {
-  convention = "ResourceGroup"
-  name       = "copilot-quickstart-rg"
-  environment = var.environment
-  project    = var.project
+resource "azurecaf_name" "resource_group" {
+  resource_type = "azurerm_resource_group"
+  prefixes = ["copilot-quickstart"]
+  random_length = 5
+  clean_input = true
 }
 
-resource "caf_naming_convention" "openai_account" {
-  convention = "OpenAIAccount"
-  name       = "copilot-quickstart-openai-account"
-  environment = var.environment
-  project    = var.project
+# Using 'general' type for these newer/misc resources until support is added
+resource "azurecaf_name" "openai_account" {
+  name       = "openai-account"
+  resource_type = "general"
+  prefixes = ["copilot-quickstart"]
+  random_length = 5
+  clean_input = true
 }
 
-resource "caf_naming_convention" "openai_deployment" {
-  convention = "OpenAIDeployment"
-  name       = "copilot-quickstart-openai-deployment"
-  environment = var.environment
-  project    = var.project
+resource "azurecaf_name" "openai_deployment" {
+  name       = "openai-deployment"
+  resource_type = "general"
+  prefixes = ["copilot-quickstart"]
+  random_length = 5
+  clean_input = true
 }
 
-resource "caf_naming_convention" "power_platform_environment" {
-  convention = "PowerPlatformEnvironment"
-  name       = "copilot-quickstart-power-platform-environment"
-  environment = var.environment
-  project    = var.project
+resource "azurecaf_name" "power_platform_environment" {
+  name       = "power-platform-environment"
+  resource_type = "general"
+  prefixes = ["copilot-quickstart"]
+  random_length = 5
+  clean_input = true
+}
+
+resource "azurecaf_name" "copilot" {
+  resource_type = "general"
+  prefixes = ["copilot-quickstart"]
+  random_length = 5
+  clean_input = true
 }
 
 #---- 2 - Set key tenant settings
@@ -74,7 +87,7 @@ resource "powerplatform_tenant_settings" "settings" {
 
 resource "powerplatform_environment" "dev" {
   location          = var.powerplatform_location
-  display_name      = caf_naming_convention.power_platform_environment.result
+  display_name      = azurecaf_name.power_platform_environment.result
   environment_type  = var.environment_type
   dataverse = {
     language_code     = var.language_code
@@ -86,32 +99,51 @@ resource "powerplatform_environment" "dev" {
 #---- 4 - Set up Azure resources ----
 
 # Define resource group
-resource "azurerm_resource_group" "Copilot Deployment Quickstart RG" {
-  name     = caf_naming_convention.resource_group.result
+resource "azurerm_resource_group" "Copilot-Deployment-Quickstart-RG" {
+  name     = azurecaf_name.resource_group.result
   location = var.azure_location
 }
 
-# Define Azure OpenAI resource
-resource "azurerm_openai_account" "Copilot Deployment Quickstart OAI Service" {
-  name                = caf_naming_convention.openai_account.result
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  sku_name            = "S0"
+locals {
+  deployment_map = {
+    dev = {
+      "chat_model_gpt_4o_2024_05_13" = {
+        name          = "deployment-${azurecaf_name.copilot.result}"
+        model_format  = "OpenAI"
+        model_name    = "gpt-4o"
+        model_version = "2024-05-13"
+        scale_type    = "Standard"
+        capacity      = 1
+      },
+    }
+  }
 }
 
-# Define an Azure OpenAI deployment
-resource "azurerm_openai_deployment" "Copilot Deployment Quickstart OAI Deployment" {
-  name                = caf_naming_convention.openai_deployment.result
-  openai_account_name = azurerm_openai_account.example.name
-  resource_group_name = azurerm_resource_group.example.name
-  model_name          = "gpt-4"
-  scale_type          = "Standard"
+module "openai" {
+  source = "Azure/openai/azurerm"
+  version = ">=0.1.3"
+  account_name = azurecaf_name.copilot.result
+  custom_subdomain_name = azurecaf_name.resource_group.result
+  resource_group_name = azurerm_resource_group.Copilot-Deployment-Quickstart-RG.name
+  location = var.azure_location
+  public_network_access_enabled = true
+  deployment = local.deployment_map[var.environment]
+  depends_on = [azurerm_resource_group.Copilot-Deployment-Quickstart-RG]
 }
 
 #---- 5 - Set up Power Platform connector ----
 
 #TODO waiting on Mateusz
 
-#---- 6 - Set up Dataverse record for the bot ----
+#---- 6 - Set up Dataverse record for the Copilot ----
+
+resource "powerplatform_data_record" "copilot" {
+  environment_id     = powerplatform_environment.dev.id
+  table_logical_name = "bot"
+  columns = {
+    name        = azurecaf_name.copilot.result
+    configuration = "{\n  \"$kind\" : \"BotConfiguration\",\n  \"publishOnCreate\" : true,\n  \"settings\" : {\n  \"GenerativeActionsEnabled\" : false,\n  },\n  \"isLightweightBot\" : true\n}"
+  }
+}
 
 #---- 7 - Future state: base bot configuration eventually needs model knowledge source ----
