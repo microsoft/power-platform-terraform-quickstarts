1fcfec115e9d631beff591b80f2d9c3de13fdd98
ianjensenisme <ianjensen@microsoft.com>
2024-08-29 13:46:48 -0700

    Security fixes

--- quickstarts/202-Copilot-deployment/azure-control-plane/main.tf
+++ quickstarts/202-Copilot-deployment/azure-control-plane/main.tf
@@ -61,6 +61,7 @@ resource "azurerm_storage_account" "Quickstart-Data-Storage" {
   min_tls_version          = "TLS1_2"
   public_network_access_enabled = false
   allow_nested_items_to_be_public = false
+  shared_access_key_enabled = false
   queue_properties  {
     logging {
         delete                = true
@@ -69,6 +70,15 @@ resource "azurerm_storage_account" "Quickstart-Data-Storage" {
         version               = "1.0"
         retention_policy_days = 14
     }
+  }
+  blob_properties {
+    delete_retention_policy {
+       days = 14
+    }
+  }
+  sas_policy {
+    expiration_period = "90.00:00:00"
+    expiration_action = "Log"
   }
 }
 
